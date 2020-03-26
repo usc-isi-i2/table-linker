38b43df68ffc15e653665752231bffcb3d5fc84d
saggu <amandeep.s.saggu@gmail.com>
2020-03-25 18:07:40 -0700

    add gt labeler

--- tl/candidate_ranking/combine_linearly.py
+++ tl/candidate_ranking/combine_linearly.py
@@ -2,7 +2,19 @@ import pandas as pd
 from tl.exceptions import RequiredInputParameterMissingException
 
 
-def combine_linearly(weights=None, output_column='ranking_score', file_path=None, df=None):
+def combine_linearly(weights, output_column='ranking_score', file_path=None, df=None):
+    """
+    combines two or more score-columns for candidate knowledge graph objects for each input cell value. Takes as input weights for columns being combined to adjust influence.
+
+    Args:
+        weights: a comma separated string, in the format <score-column-1>:<weight-1>,<score-column-2>:<weight-2>,... representing weights for each score-column. Default weight for each score-column is 1.0.
+        output_column: the output column name where the linearly combined scores will be stored. Default is ranking_score
+        file_path: input file path
+        df: input dataframe
+
+    Returns: a dataframe in ranking score file format
+
+    """
     if file_path is None and df is None:
         raise RequiredInputParameterMissingException(
             'One of the input parameters is required: {} or {}'.format('file_path', 'df'))
