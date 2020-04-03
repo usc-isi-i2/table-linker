a4f63d7ea61a9ac88cbab5316bc552943a7413ed
saggu <amandeep.s.saggu@gmail.com>
2020-04-03 09:41:27 -0700

    add gt for clean unittests

--- tl/unittests/test_clean.py
+++ tl/unittests/test_clean.py
@@ -13,33 +13,26 @@ class TestClean(unittest.TestCase):
 
     def test_clean_default(self):
         odf = preprocess.clean('label', df=self.input_csv)
-
-        self.assertTrue('label_clean' in odf.columns.values)
-        self.assertEqual(odf.at[487, 'label_clean'], "L'Arc-en-Ciel")
-
-        self.assertEqual(odf.at[680, 'label_clean'], "0 to 100   The Catch Up")
-        print(odf.at[677, 'label_clean'])
-        print(odf.at[430, 'label_clean'])
+        gdf = pd.read_csv('{}/data/canonical_default_gt.csv'.format(parent_path), dtype=object)
+        for i in range(len(odf)):
+            self.assertEqual(odf.at[i, 'label_clean'], gdf.at[i, 'label_clean'])
 
     def test_clean_replace_by_space_false(self):
         odf = preprocess.clean('label', df=self.input_csv, output_column='clean_labels', replace_by_space=False)
-
+        gdf = pd.read_csv('{}/data/canonical_replace_by_space_false_gt.csv'.format(parent_path), dtype=object)
         self.assertTrue('clean_labels' in odf.columns.values)
 
-        self.assertEqual(odf.at[680, 'clean_labels'], "0 to 100  The Catch Up")
+        for i in range(len(odf)):
+            self.assertEqual(odf.at[i, 'clean_labels'], gdf.at[i, 'clean_labels'])
 
     def test_clean_keep_original_true(self):
         odf = preprocess.clean('label', df=self.input_csv, keep_original=True)
-
-        self.assertEqual(odf.at[680, 'label_clean'], "0 to 100 / The Catch Up|0 to 100   The Catch Up")
-        self.assertEqual(odf.at[624, 'label_clean'], "Chase|Chase")
-        self.assertEqual(odf.at[66, 'label_clean'], "Y.M.C.A.|Y.M.C.A.")
+        gdf = pd.read_csv('{}/data/canonical_keep_original_gt.csv'.format(parent_path), dtype=object)
+        for i in range(len(odf)):
+            self.assertEqual(odf.at[i, 'label_clean'], gdf.at[i, 'label_clean'])
 
     def test_clean_symbols(self):
         odf = preprocess.clean('label', df=self.input_csv, symbols=".'")
-
-        self.assertEqual(odf.at[350, 'label_clean'], "L Arc-en-Ciel")
-        self.assertEqual(odf.at[434, 'label_clean'], "Guns N  Roses")
-        self.assertEqual(odf.at[447, 'label_clean'], "The Go-Go s")
-        self.assertEqual(odf.at[448, 'label_clean'], "Will i am")
-        self.assertEqual(odf.at[66, 'label_clean'], "Y M C A ")
+        gdf = pd.read_csv('{}/data/canonical_symbols_gt.csv'.format(parent_path), dtype=object)
+        for i in range(len(odf)):
+            self.assertEqual(odf.at[i, 'label_clean'], gdf.at[i, 'label_clean'])
