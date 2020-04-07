474cffada93dde7e05572c631c215f029105d9e2
saggu <amandeep.s.saggu@gmail.com>
2020-04-06 18:28:06 -0700

    handle chaining of CG commands

--- tl/candidate_generation/utility.py
+++ tl/candidate_generation/utility.py
@@ -13,18 +13,24 @@ class Utility(object):
         df_columns = df.columns
 
         if self.ffv.is_canonical_file(df):
-            candidates_format = self.create_cfd(df, df_columns, column, size, properties, method, lower_case)
+            candidates_format = self.create_cfd_canonical(df, df_columns, column, size, properties, method, lower_case)
 
             return pd.DataFrame(candidates_format)
 
         elif self.ffv.is_candidates_file(df):
             grouped = df.groupby(by=['column', 'row', column])
+            relevant_columns = [c for c in df_columns if c not in ['kg_id', 'kg_labels', 'method', 'retrieval_score']]
             for key_tuple, gdf in grouped:
-                candidates_format.extend(self.create_cfd(gdf, key_tuple, column, size, properties, method, lower_case))
-            return pd.concat(df, pd.DataFrame(candidates_format))
+                gdf.reset_index(inplace=True)
+                tuple = ((c, gdf.at[0, c]) for c in relevant_columns)
 
-    def create_cfd(self, df, relevant_columns, column, size, properties, method, lower_case):
+                candidates_format.extend(
+                    self.create_cfd_candidates(tuple, column, size, properties, method, lower_case))
+            return pd.concat([df, pd.DataFrame(candidates_format)])
+
+    def create_cfd_canonical(self, df, relevant_columns, column, size, properties, method, lower_case):
         candidates_format = list()
+
         for i, row in df.iterrows():
             candidate_dict = self.es.search_term_candidates(row[column], size, properties, method,
                                                             lower_case=lower_case)
@@ -32,7 +38,8 @@ class Utility(object):
             if not candidate_dict:
                 cf_dict = {}
                 for key in relevant_columns:
-                    cf_dict[key] = row[key]
+                    if key not in ['kg_id', 'kg_labels', 'method', 'retrieval_score']:
+                        cf_dict[key] = row[key]
 
                 cf_dict['kg_id'] = ""
                 cf_dict['kg_labels'] = ""
@@ -43,10 +50,46 @@ class Utility(object):
                 for kg_id in candidate_dict:
                     cf_dict = {}
                     for key in relevant_columns:
-                        cf_dict[key] = row[key]
+                        if key not in ['kg_id', 'kg_labels', 'method', 'retrieval_score']:
+                            cf_dict[key] = row[key]
+
                     cf_dict['kg_id'] = kg_id
                     cf_dict['kg_labels'] = candidate_dict[kg_id]['label_str']
                     cf_dict['method'] = method
                     cf_dict['retrieval_score'] = candidate_dict[kg_id]['score']
                     candidates_format.append(cf_dict)
+        return candidates_format
+
+    def create_cfd_candidates(self, key_tuple, column, size, properties, method, lower_case):
+        candidates_format = list()
+
+        _ = {}
+        for k in key_tuple:
+            _[k[0]] = k[1]
+
+        candidate_dict = self.es.search_term_candidates(_[column], size, properties, method,
+                                                        lower_case=lower_case)
+
+        if not candidate_dict:
+            cf_dict = {}
+
+            for k in _:
+                cf_dict[k] = _[k]
+
+            cf_dict['kg_id'] = ""
+            cf_dict['kg_labels'] = ""
+            cf_dict['method'] = method
+            cf_dict['retrieval_score'] = 0.0
+            candidates_format.append(cf_dict)
+        else:
+            for kg_id in candidate_dict:
+                cf_dict = {}
+                for k in _:
+                    cf_dict[k] = _[k]
+
+                cf_dict['kg_id'] = kg_id
+                cf_dict['kg_labels'] = candidate_dict[kg_id]['label_str']
+                cf_dict['method'] = method
+                cf_dict['retrieval_score'] = candidate_dict[kg_id]['score']
+                candidates_format.append(cf_dict)
         return candidates_format
