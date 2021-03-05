c805249aa3a4fae0994796a2a22e847ae3611057
saggu <amandeep.s.saggu@gmail.com>
2021-03-04 18:17:25 -0800

    add auxiliary fields option

--- tl/cli/get-exact-matches.py
+++ tl/cli/get-exact-matches.py
@@ -32,6 +32,16 @@ def add_arguments(parser):
                         help='the output column name where the normalized scores will be stored.'
                              'Default is retrieval_score')
 
+    parser.add_argument('--auxiliary-fields', action='store', type=str, dest='auxiliary_fields', default=None,
+                        help='A comma separated string of auxiliary field names in the elasticsearch.'
+                             'A file will be created for each of the specified field at the location specified by'
+                             ' the `--auxiliary-folder` option. If this option is specified then,'
+                             ' `--auxiliary-folder` must also be specified.')
+
+    parser.add_argument('--auxiliary-folder', action='store', type=str, dest='auxiliary_folder', default=None,
+                        help='location where the auxiliary files for auxiliary fields will be stored.'
+                             'If this option is specified then `--auxiliary-fields` must also be specified.')
+
     parser.add_argument('input_file', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
 
 
@@ -39,6 +49,12 @@ def run(**kwargs):
     from tl.candidate_generation import get_exact_matches
     import pandas as pd
     try:
+        auxiliary_fields = kwargs.get('auxiliary_fields', None)
+        auxiliary_folder = kwargs.get('auxiliary_folder', None)
+        if (auxiliary_folder is not None and auxiliary_fields is None) or (
+                auxiliary_folder is None and auxiliary_fields is not None):
+            raise Exception("Both the options `--auxiliary-fields` and `--auxiliary-folder` have to be specified "
+                            "if either one is specified")
         df = pd.read_csv(kwargs['input_file'], dtype=object)
         em = get_exact_matches.ExactMatches(es_url=kwargs['url'], es_index=kwargs['index'], es_user=kwargs['user'],
                                             es_pass=kwargs['password'], output_column_name=kwargs['output_column_name'])
