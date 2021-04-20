1019da8160a850a1b8c87d17758b5f68afa7eb11
saggu <amandeep.s.saggu@gmail.com>
2021-04-19 17:27:44 -0700

    handle multiple columns, show top1 resultonly

--- tl/evaluation/join.py
+++ tl/evaluation/join.py
@@ -7,13 +7,16 @@ class Join(object):
         columns_to_wikify = list(df['column'].unique())
         output = []
         result_dict = self.create_result_dict(df, ranking_score_column, extra_info)
+
+        i_cols = i_df.columns
         for i, row in i_df.iterrows():
             for column_to_wikify in columns_to_wikify:
                 key = f"{column_to_wikify}_{i}"
                 result = result_dict.get(key, None)
                 if result:
                     for k in result:
-                        row[k] = result[k]
+                        c_name = i_cols[int(column_to_wikify)]
+                        row[f"{c_name}_{k}"] = result[k]
             output.append(row)
         return pd.DataFrame(output)
 
@@ -22,12 +25,14 @@ class Join(object):
         _ = {}
         for i, row in df.iterrows():
             key = f"{row['column']}_{row['row']}"
-            _[key] = {
-                'kg_id': row['kg_id'],
-                'kg_label': row['kg_label'],
-                'score': row[ranking_score_column]
-            }
-            if extra_info:
-                _[key]['kg_aliases'] = row['kg_aliases']
-                _[key]['kg_descriptions'] = row['kg_descriptions']
+            if key not in _:
+                _[key] = {
+                    'kg_id': row['kg_id'],
+                    'kg_label': row['kg_labels'],
+                    'score': row[ranking_score_column]
+                }
+                if extra_info:
+                    _[key]['kg_aliases'] = row['kg_aliases']
+                    _[key]['kg_descriptions'] = row['kg_descriptions']
+
         return _
