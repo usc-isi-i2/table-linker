72c574063121e65600b4b86e3b46a1062c5cc4b5
HardiRathod <33089230+HardiRathod@users.noreply.github.com>
2021-06-07 17:36:17 +0530

    Update and rename context_match.py to context-match.py

--- tl/cli/context-match.py
+++ tl/cli/context-match.py
@@ -12,16 +12,13 @@ def parser():
 
 
 def add_arguments(parser):
-    from tl.utility.utility import Utility
-    from kgtk.cli.text_embedding import ALL_EMBEDDING_MODELS_NAMES
     # input file
-    parser.add_argument('input_file', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
-    # debug
-    parser.add_argument('context_file', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument('input-file', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument('--context-file', type=str, dest='context-file', required=True, help = "The file is used to look up context values for matching.")
     parser.add_argument('--debug', action='store_true', help="if set, an kgtk debug logger will be saved at home directory. Debug adds two new columns to the output denoting the properties matched and the respective similarities.")
-    parser.add_argument('--sim_string', action='store', type=float, dest='sim_string', default=0.75,
+    parser.add_argument('--sim-string-threshold', action='store', type=float, dest='sim-string-threshold', default=0.75,
                         help='The minimum threshold for similarity with input context for string matching.')
-    parser.add_argument('--sim_quantity', action='store', type=float, dest='sim_quantity', default=0.85,
+    parser.add_argument('--sim-quantity-threshold', action='store', type=float, dest='sim-quantity-threshold', default=0.85,
                         help='The minimum threshold for similarity with input context for quantity matching.')
 
     # output
@@ -30,12 +27,12 @@ def add_arguments(parser):
 
 def run(**kwargs):
     try:
-        from tl.features.context_match import match
-        input_file_path = kwargs.pop("input_file")
-        context_file_path = kwargs.pop("context_file")
-        obj = match(input_file_path, context_file_path, kwargs)
-        obj.divide_by_column()
+        from tl.features.context_match import Match
+        input_file_path = kwargs.pop("input-file")
+        context_file_path = kwargs.pop("context-file")
+        obj = Match(input_file_path, context_file_path, kwargs)
+        obj.process_data_by_column()
     except:
-        message = 'Command: context_matching\n'
+        message = 'Command: context-match\n'
         message += 'Error Message:  {}\n'.format(traceback.format_exc())
         raise tl.exceptions.TLException(message)
