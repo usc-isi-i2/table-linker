465a75e15b015967f724f6bfec4670f85077b3b5
HardiRathod <33089230+HardiRathod@users.noreply.github.com>
2021-06-10 01:04:40 +0530

    Update context-match.py

--- tl/cli/context-match.py
+++ tl/cli/context-match.py
@@ -2,23 +2,24 @@ import typing
 import argparse
 import sys
 import traceback
+import pandas as pd
 import tl.exceptions
 
 
 def parser():
     return {
-        'help': 'Match the context values to the properties and add the score of the match.'
+        'help': 'The context-match function adds a feature column by matching the context values of each candidate to its properties and calculating the score based on the match.'
     }
 
 
 def add_arguments(parser):
     # input file
-    parser.add_argument('input-file', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
-    parser.add_argument('--context-file', type=str, dest='context-file', required=True, help = "The file is used to look up context values for matching.")
+    parser.add_argument('input_file', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument('--context-file', type=str, dest='context_file', required=True, help = "The file is used to look up context values for matching.")
     parser.add_argument('--debug', action='store_true', help="if set, an kgtk debug logger will be saved at home directory. Debug adds two new columns to the output denoting the properties matched and the respective similarities.")
-    parser.add_argument('--similarity-string-threshold', action='store', type=float, dest='sim-string-threshold', default=0.75,
+    parser.add_argument('--similarity-string-threshold', action='store', type=float, dest='similarity_string_threshold', default=0.75,
                         help='The minimum threshold for similarity with input context for string matching.')
-    parser.add_argument('--similarity-quantity-threshold', action='store', type=float, dest='sim-quantity-threshold', default=0.85,
+    parser.add_argument('--similarity-quantity-threshold', action='store', type=float, dest='similarity_quantity_threshold', default=0.75,
                         help='The minimum threshold for similarity with input context for quantity matching.')
 
     # output
@@ -27,11 +28,12 @@ def add_arguments(parser):
 
 def run(**kwargs):
     try:
-        from tl.features.context_match import Match
-        input_file_path = kwargs.pop("input-file")
-        context_file_path = kwargs.pop("context-file")
-        obj = Match(input_file_path, context_file_path, kwargs)
-        obj.process_data_by_column()
+        from tl.features.context_match import Match_context
+        input_file_path = kwargs.pop("input_file")
+        context_file_path = kwargs.pop("context_file")
+        obj = Match_context(input_file_path, context_file_path, kwargs)
+        result_df = obj.process_data_by_column()
+        result_df.to_csv(sys.stdout, index = False)
     except:
         message = 'Command: context-match\n'
         message += 'Error Message:  {}\n'.format(traceback.format_exc())
