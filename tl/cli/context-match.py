8a6946b1d5ed51143e992d16314fa4fd8bf9e6b5
saggu <amandeep.s.saggu@gmail.com>
2021-06-14 15:43:49 -0700

    add default values in help messages

--- tl/cli/context-match.py
+++ tl/cli/context-match.py
@@ -1,8 +1,8 @@
-import typing
+import traceback
+
 import argparse
 import sys
-import traceback
-import pandas as pd
+
 import tl.exceptions
 
 
@@ -15,16 +15,26 @@ def parser():
 def add_arguments(parser):
     # input file
     parser.add_argument('input_file', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
-    parser.add_argument('--context-file', type=str, dest='context_file', required=True, help = "The file is used to look up context values for matching.")
-    parser.add_argument('--debug', action='store_true', help="if set, an kgtk debug logger will be saved at home directory. Debug adds two new columns to the output denoting the properties matched and the respective similarities.")
-    parser.add_argument('--similarity-string-threshold', action='store', type=float, dest='similarity_string_threshold', default=0.75,
-                        help='The minimum threshold for similarity with input context for string matching.')
-    parser.add_argument('--similarity-quantity-threshold', action='store', type=float, dest='similarity_quantity_threshold', default=0.85,
-                        help='The minimum threshold for similarity with input context for quantity matching.')
+    parser.add_argument('--context-file', type=str, dest='context_file', required=True,
+                        help="The file is used to look up context values for matching.")
+    parser.add_argument('--debug', action='store_true',
+                        help="if set, an kgtk debug logger will be saved at home directory. "
+                             "Debug adds two new columns to the output denoting the properties matched and the "
+                             "respective similarities.")
+    parser.add_argument('--similarity-string-threshold', action='store', type=float, dest='similarity_string_threshold',
+                        default=0.75,
+                        help='The minimum threshold for similarity with input context for string matching. '
+                             'Default: 0.75')
+    parser.add_argument('--similarity-quantity-threshold', action='store', type=float,
+                        dest='similarity_quantity_threshold', default=0.85,
+                        help='The minimum threshold for similarity with input context for quantity matching. '
+                             'Default: 0.85')
 
     # output
-    parser.add_argument('-o', '--output-column-name', action='store', dest='output_column', default = "context_score",
-                        help='The output column is the named column of the score for the matches computed for the context.')
+    parser.add_argument('-o', '--output-column-name', action='store', dest='output_column', default="context_score",
+                        help='The output column is the named column of the score for the matches '
+                             'computed for the context.')
+
 
 def run(**kwargs):
     try:
@@ -33,7 +43,7 @@ def run(**kwargs):
         context_file_path = kwargs.pop("context_file")
         obj = MatchContext(input_file_path, context_file_path, kwargs)
         result_df = obj.process_data_by_column()
-        result_df.to_csv(sys.stdout, index = False)
+        result_df.to_csv(sys.stdout, index=False)
     except:
         message = 'Command: context-match\n'
         message += 'Error Message:  {}\n'.format(traceback.format_exc())
