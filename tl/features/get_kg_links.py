e1f7efea30bd9c20473e48e9730789f283c6adb1
saggu <amandeep.s.saggu@gmail.com>
2021-06-23 19:01:10 -0700

    add GT as last row if GT is not in top k

--- tl/features/get_kg_links.py
+++ tl/features/get_kg_links.py
@@ -24,23 +24,53 @@ def get_kg_links(score_column, file_path=None, df=None, label_column='label', to
     topk_df = df.groupby(['column', 'row']).apply(lambda x: x.sort_values([score_column], ascending=False)) \
         .reset_index(drop=True)
 
+    is_gt_present = 'evaluation_label' in df.columns
+
     final_list = []
-    grouped_obj = topk_df.groupby(['row', 'column'])
-    for cell in grouped_obj:
-        cell[1].drop_duplicates(subset='kg_id', inplace=True)
-        if not(k_rows):
+    grouped_obj = topk_df.groupby(['column', 'row'])
+    for key, grouped in grouped_obj:
+        grouped['rank'] = list(grouped[score_column].rank(method='first', ascending=False).astype(int))
+
+        grouped.drop_duplicates(subset='kg_id', inplace=True)
+        new_top_k = top_k
+        gt_rank = -1
+        if is_gt_present:
+            gt_rank_values = grouped[grouped['evaluation_label'] == 1]['rank'].values
+            if len(gt_rank_values) > 0:
+                gt_rank = gt_rank_values[0]
+            if gt_rank > top_k:
+                new_top_k -= 1
+
+        if not (k_rows):
             _ = {}
-            _['column'] = cell[0][1]
-            _['row'] = cell[0][0]
-            _['label'] = cell[1][label_column].unique()[0]
-            _['kg_id'] = '|'.join(list(cell[1]['kg_id'])[:top_k])
-            _['kg_labels'] = '|'.join(list(cell[1]['kg_labels'])[:top_k])
-            _['kg_descriptions'] = '|'.join(list(cell[1]['kg_descriptions'])[:top_k])
-            _['kg_aliases'] = '|'.join(list(cell[1]['kg_aliases'])[:top_k])
-            _['ranking_score'] = '|'.join([str(score) for score in list(cell[1][score_column])[:top_k]])
+
+            kg_ids = list(grouped['kg_id'])[:new_top_k]
+            kg_labels = list(grouped['kg_labels'])[:new_top_k]
+            kg_descriptions = list(grouped['kg_descriptions'])[:new_top_k]
+            kg_aliases = list(grouped['kg_aliases'])[:new_top_k]
+            scores = [str(score) for score in list(grouped[score_column])[:new_top_k]]
+            if gt_rank > top_k:
+                kg_ids.extend(list(grouped['kg_id'])[gt_rank])
+                kg_labels.extend(list(grouped['kg_labels'])[gt_rank])
+                kg_descriptions.extend(list(grouped['kg_descriptions'])[gt_rank])
+                kg_aliases.extend(list(grouped['kg_aliases'])[gt_rank])
+                scores.append(str(list(grouped[score_column])[gt_rank]))
+
+            _['column'] = key[0]
+            _['row'] = key[1]
+            _['label'] = grouped[label_column].unique()[0]
+            _['kg_id'] = '|'.join(kg_ids)
+            _['kg_labels'] = '|'.join(kg_labels)
+            _['kg_descriptions'] = '|'.join(kg_descriptions)
+            _['kg_aliases'] = '|'.join(kg_aliases)
+            _['ranking_score'] = '|'.join(scores)
+
             final_list.append(_)
         else:
-            topk_df_row = cell[1].head(top_k)
+            if gt_rank > top_k:
+                topk_df_row = pd.concat([grouped.head(new_top_k), grouped[grouped['rank'] == gt_rank]])
+            else:
+                topk_df_row = grouped.head(new_top_k)
             final_list.extend(topk_df_row.to_dict(orient='records'))
 
     odf = pd.DataFrame(final_list)
