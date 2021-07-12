29f8508bae1e8d8e09be7895f366c23d3328959b
saggu <amandeep.s.saggu@gmail.com>
2021-07-12 11:42:24 -0700

    one more unit test for extra columns

--- tl/unittests/test_canonicalize.py
+++ tl/unittests/test_canonicalize.py
@@ -40,3 +40,18 @@ class TestCanonicalize(unittest.TestCase):
         self.assertTrue('context' in odf.columns)
         self.assertEqual(odf.iloc[116, 3], "B. Robison|George Strait|How 'bout Them Cowgirls")
         self.assertEqual(odf.iloc[91, 3], "B. Nowell|Paul Leary|Doin' Time")
+
+    def test_canonicalize_with_additional_columns(self):
+        odf = preprocess.canonicalize('col0,col3', df=self.input_csv, add_context=True, file_name='input.csv')
+        self.assertTrue(self.ffv.is_canonical_file(odf))
+        self.assertTrue('context' in odf.columns)
+        self.assertTrue('filename' in odf.columns)
+        self.assertTrue('column-id' in odf.columns)
+        self.assertEqual(odf.iloc[116, 3], "B. Robison|George Strait|How 'bout Them Cowgirls")
+        self.assertEqual(odf.iloc[91, 3], "B. Nowell|Paul Leary|Doin' Time")
+        self.assertEqual(odf.iloc[34, 4], "input.csv")
+        self.assertEqual(odf.iloc[34, 5], "input.csv-0")
+        self.assertEqual(odf.iloc[200, 5], "input.csv-3")
+        grouped = odf.groupby(['column'])
+        for key, gdf in grouped:
+            self.assertEqual(len(gdf), 137)
