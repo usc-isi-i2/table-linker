dc8a8668b5320c6d83c408453c40d9f49742f56d
Sriramkumar Thamizharasan <sriramkumar1998@gmail.com>
2021-07-26 18:36:41 -0400

    Modified pseudo gt computation

--- tl/features/create_pseudo_gt.py
+++ tl/features/create_pseudo_gt.py
@@ -2,6 +2,7 @@ import pandas as pd
 from tl.exceptions import RequiredColumnMissingException
 from tl.file_formats_validator import FFV
 from tl.exceptions import UnsupportTypeError
+import sys
 
 
 def create_pseudo_gt(df: pd.DataFrame, column_thresholds: str,
@@ -15,10 +16,30 @@ def create_pseudo_gt(df: pd.DataFrame, column_thresholds: str,
                     "The input column {} does not exist"
                     " in given data.".format(column))
 
-            df.loc[(df[column].astype(float) >= float(threshold)),
-                   output_column] = 1
+            if threshold=="median":
+                grouped = df.groupby(by=["column", "row"])
+                for _, gdf in grouped:
+                    gdf.loc[((gdf[column]==gdf[column].max()) & 
+                           (gdf[column].astype(float) >= 
+                            gdf[column].astype(float).median())), 
+                           output_column]=1
+                    df.loc[gdf.index[gdf[output_column].astype(float)==1],
+                           output_column]=1
+            elif threshold=="mean":
+                grouped = df.groupby(by=["column", "row"])
+                for _, gdf in grouped:
+                    gdf.loc[((gdf[column]==gdf[column].max()) & 
+                           (gdf[column].astype(float) >= 
+                            gdf[column].astype(float).mean())), 
+                           output_column]=1
+                    df.loc[gdf.index[gdf[output_column].astype(float)==1],
+                           output_column]=1
+            else:
+                df.loc[(df[column].astype(float) >= float(threshold)),
+                       output_column] = 1
 
-        df[output_column] = df[output_column].fillna(0)
+        df[output_column] = df[output_column].fillna(-1)
+        df =  df.astype({output_column: int})
         return df
     else:
         raise UnsupportTypeError("The input file is not a candidates file!")
