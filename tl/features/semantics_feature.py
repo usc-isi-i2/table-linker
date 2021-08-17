5aa4dea0eb6af954db8c590cab7cd75db23217fa
saggu <amandeep.s.saggu@gmail.com>
2021-08-17 14:12:06 -0700

    add ignore column option

--- tl/features/semantics_feature.py
+++ tl/features/semantics_feature.py
@@ -6,8 +6,15 @@ HC_CANDIDATE = 'hc_candidate'
 
 
 class SemanticsFeature(object):
-    def __init__(self, output_column, feature_file, feature_name, total_docs, pagerank_column, retrieval_score_column,
-                 input_file=None, df=None):
+    def __init__(self, output_column: str,
+                 feature_file: str,
+                 feature_name: str,
+                 total_docs: float,
+                 pagerank_column: str,
+                 retrieval_score_column: str,
+                 hc_column: str,
+                 input_file: str = None,
+                 df: pd.DataFrame = None):
 
         if df is None and input_file is None:
             raise RequiredInputParameterMissingException(
@@ -24,7 +31,8 @@ class SemanticsFeature(object):
         self.output_col_name = output_column
         self.pagerank_column = pagerank_column
         self.retrieval_score_column = retrieval_score_column
-        self.N = float(total_docs)
+        self.N = total_docs
+        self.hc_column = hc_column
         self.utils = Utility()
 
         self.multiply_pgr_rts()
@@ -39,6 +47,16 @@ class SemanticsFeature(object):
 
         self.hc_classes = self.create_hc_classes_set()
 
+        grouped = self.input_df.groupby(['column', 'row'])
+        table_lens = {}
+
+        for key, gdf in grouped:
+            if key[0] not in table_lens:
+                table_lens[key[0]] = 0
+            table_lens[key[0]] += 1
+
+        self.table_lengths = table_lens
+
     def create_hc_classes_set(self):
         hc_classes = set()
         for candidate in self.hc_candidates:
@@ -46,8 +64,11 @@ class SemanticsFeature(object):
         return hc_classes
 
     def find_hc_candidates(self):
-        self.label_high_confidence_candidates()
-        data = self.input_df[self.input_df[HC_CANDIDATE] == 1]
+        if self.hc_column is not None:
+            data = self.input_df[self.input_df[self.hc_column].astype(int) == 1]
+        else:
+            self.label_high_confidence_candidates()
+            data = self.input_df[self.input_df[HC_CANDIDATE] == 1]
         hc_candidates = set(data['kg_id'])
         return hc_candidates
 
@@ -88,11 +109,12 @@ class SemanticsFeature(object):
         top_5_col_name = f"top5_{self.output_col_name}"
 
         candidate_x_i, alpha_j = self.compute_xi_alphaj()
-        hc_classes_idf = self.utils.normalize_idf_high_confidence_classes(self.input_df, HC_CANDIDATE,
+        _hc_column = self.hc_column if self.hc_column is not None else HC_CANDIDATE
+        hc_classes_idf = self.utils.normalize_idf_high_confidence_classes(self.input_df, _hc_column,
                                                                           self.feature_dict, self.feature_idf_dict)
 
         for _qnode, column, row, hc_candidate in zip(self.input_df['kg_id'], self.input_df['column'],
-                                                     self.input_df['row'], self.input_df[HC_CANDIDATE]):
+                                                     self.input_df['row'], self.input_df[_hc_column]):
 
             top_5_features_candidate = {}
 
@@ -118,7 +140,10 @@ class SemanticsFeature(object):
         return self.input_df
 
     def compute_xi_alphaj(self) -> (dict, dict):
-        data = self.input_df[self.input_df[HC_CANDIDATE] == 1]
+        if self.hc_column is not None:
+            data = self.input_df[self.input_df[self.hc_column] == 1]
+        else:
+            data = self.input_df[self.input_df[HC_CANDIDATE] == 1]
 
         classes_count = {}
         candidate_x_i = {}
