1c0919bd8a2dc18db413bec31d3c86c016446ed9
saggu <amandeep.s.saggu@gmail.com>
2021-08-23 14:05:03 -0700

    add minimum cells

--- tl/features/kth_percentile.py
+++ tl/features/kth_percentile.py
@@ -11,7 +11,8 @@ class KthPercentile(object):
                  input_file: str = None,
                  output_column: str = 'kth_percenter',
                  k_percentile: str = 'mean',
-                 ignore_column: str = None):
+                 ignore_column: str = None,
+                 minimum_cells: int = None):
         if df is None and input_file is None:
             raise RequiredInputParameterMissingException(
                 'One of the input parameters is required: {} or {}'.format("input_file", "df"))
@@ -30,6 +31,7 @@ class KthPercentile(object):
         self.output_column = output_column
         self.k_percentile = k_percentile
         self.ignore_column = ignore_column
+        self.minimum_cells = minimum_cells
 
     def process(self, column):
         output = []
@@ -38,20 +40,72 @@ class KthPercentile(object):
         for c, gdf in self.input_df.groupby(by=['column']):
 
             _gdf = gdf if self.ignore_column is None else gdf[gdf[self.ignore_column].astype(int) == 0]
-            _k_percentile = None
-            if not is_k_p_number:
-                # kth percentile is either mean or median
-                _k_percentile = _gdf[column].mean() if self.k_percentile == 'mean' else _gdf[column].median()
-            else:  # kth percentile is a number
-                _ = float(self.k_percentile)
-                _kth = 1.0 - _ if _ != 1.0 else _
-                assert 0.0 <= _kth <= 1.0, "--k-percentile should be a number between [0.0, 1.0]," \
-                                           " or a string ∈ {mean, median}"
-                _k_percentile = float(_gdf[column].quantile(_kth))
+            if len(_gdf[column].unique()) == 1:
+                gdf.loc[gdf['ignore_candidate'] == 0, self.output_column] = 1
+                output.append(gdf)
+            else:
+                _k_percentile = None
+                if not is_k_p_number:
+                    # kth percentile is either mean or median
+                    _k_percentile = _gdf[column].mean() if self.k_percentile == 'mean' else _gdf[column].median()
+                else:  # kth percentile is a number
+                    _ = float(self.k_percentile)
+                    _kth = 1.0 - _ if _ != 1.0 else _
+                    assert 0.0 <= _kth <= 1.0, "--k-percentile should be a number between [0.0, 1.0]," \
+                                               " or a string ∈ {mean, median}"
+                    _k_percentile = float(_gdf[column].quantile(_kth))
 
-            gdf.loc[gdf[column] >= _k_percentile, self.output_column] = 1
-            output.append(gdf)
+                gdf.loc[gdf[column] >= _k_percentile, self.output_column] = 1
+                output.append(self.add_more_cells(gdf, column, c))
+
         return pd.concat(output)
+
+    def add_more_cells(self, output_df: pd.DataFrame, score_column: str, column) -> pd.DataFrame:
+        if self.minimum_cells is None:
+            return output_df
+
+        # check to see if we have enough cells with candidates in kth_percenter
+        kth_p_cells = output_df[output_df[self.output_column] == 1].groupby(by=['row'])
+        if len(kth_p_cells) >= self.minimum_cells:
+            return output_df
+
+        cell_bucket = set()
+        seen_labels = dict()
+
+        for r, gdf in kth_p_cells:
+            key = f"{column}_{r}"
+            cell_bucket.add(key)
+            label = gdf['label_clean'].to_list()[0]
+            if label not in seen_labels:
+                seen_labels[label] = key
+
+        kth_percenter_df = output_df[output_df[self.output_column] == 1]
+        nonkth_percenter_df = output_df[output_df[self.output_column] == 0]
+
+        sorted_df = nonkth_percenter_df.sort_values(by=score_column, ascending=False)
+
+        scores = []
+        for column, row, label, context_score, kth_percenter in zip(sorted_df['column'], sorted_df['row'],
+                                                                    sorted_df['label_clean'], sorted_df[score_column],
+                                                                    sorted_df[self.output_column]):
+            if len(cell_bucket) >= self.minimum_cells:
+                break
+
+            cell_bucket_key = f"{column}_{row}"
+
+            if cell_bucket_key not in cell_bucket and \
+                    (seen_labels.get(label) is None or cell_bucket_key == seen_labels[label]):
+                scores.append(1)
+                cell_bucket.add(cell_bucket_key)
+                if label not in seen_labels:
+                    seen_labels[label] = cell_bucket_key
+            else:
+                scores.append(0)
+        scores_to_extend = [0] * (len(sorted_df) - len(scores))
+        scores.extend(scores_to_extend)
+        sorted_df[self.output_column] = scores
+        assert len(output_df) == len(sorted_df) + len(kth_percenter_df), "You messed up!"
+        return pd.concat([kth_percenter_df, sorted_df]).sort_values(by=['column', 'row'])
 
     def is_k_percentile_number(self):
         try:
